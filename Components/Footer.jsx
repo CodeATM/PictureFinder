82194f582b38d0c13fc2e083fdc4c4f6f757487c
TIMILEYIN AWE <atmnation2004@gmail.com>
2023-11-28 02:01:51 -0800

    footer

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -3,32 +3,32 @@ import { FiImage, FiTwitter, FiGithub, FiGlobe } from "react-icons/fi";
 
 const Footer = () => {
   return (
-    <footer className="bg-black text-white">
-      <div className="container py-10 px-10">
-        <div className="flex justify-between items-center flex-col md:flex-row space-y-4 md:space-y-0">
-          <div className="md:text-start text-center space-y-2 md:w-[50%]">
-            <span className="text-2xl text-white font-bold flex justify-center md:justify-start">
+    <footer className="">
+      <div className="">
+        <div className="">
+          <div className="">
+            <span className="">
               <FiImage />
             </span>
-            <h1 className="font-bold text-2xl tracking-wide text-white">Picfinder</h1>
-            <p className=" font-semibold text-white">
+            <h1 className="">Picfinder</h1>
+            <p className="">
               Lorem ipsum, dolor sit amet consectetur adipisicing elit.
               Obcaecati ut et nobis ex, accusamus impedit aliquam beatae! Est
               vitae culpa modi.
             </p>
           </div>
 
-          <div className="space-y-2">
-            <h1 className="text-xl font-semibold">Connect with us</h1>
-            <div className="flex space-x-1 justify-center">
-              <div className="hover:bg-slate-400 p-2 rounded-full text-white">
+          <div className="">
+            <h1 className="">Connect with us</h1>
+            <div className="">
+              <div className="">
                 <FiGithub />
               </div>
 
-              <div className="hover:bg-slate-400 p-2 rounded-full text-white">
+              <div className="">
                 <FiGlobe />
               </div>
-              <div className="hover:bg-slate-400 p-2 rounded-full text-white">
+              <div className="">
                 <FiTwitter />
               </div>
             </div>
